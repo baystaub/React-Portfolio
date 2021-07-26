dcc16ea9d4b6d8a3a457ef71bc8955296f4c6fca
baystaub <staubbm@gmail.com>
2021-07-26 16:56:39 -0600

    hoping that it will deploy this time

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Route, Redirect, HashRouter } from "react-router-dom";
 import Navigation from "../../components/Navigation";
 import About from "../../components/About";
-// import Portfolio from "../../components/Portfolio";
 import Contact from '../../components/contact';
 
 
@@ -18,7 +17,6 @@ class Header extends Component {
           <div className="content">
             <Route exact path="/" render={() => <Redirect to="/about" />} />
             <Route path="/about" component={About} />
-            {/* <Route path="/portfolio" component={Portfolio} /> */}
             <Route path="/contact" component={Contact}/>
             {/* <Route path="/resume" component={Resume}/> */}
           </div>
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
 
     return (
-        <footer classname ='footer bg-dark fixxed-bottom'>
+        <footer classname ='footer bg-dark fixed-bottom'>
             <a href="https://github.com/baystaub" ><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
             <a href="https://www.linkedin.com/in/bailey-staub-9a169b205/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
         </footer>
